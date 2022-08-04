0afbbc687453fedec496bd63d30eb9022430677d
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-08-04 15:47:13 +0100

    complete activities.js

--- db/scripts/createTable.js
+++ db/scripts/createTable.js
@@ -3,6 +3,8 @@ import { query } from "../index.js"
 const sqlString = `CREATE TABLE IF NOT EXISTS activities (
        id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
        name VARCHAR(35),
+       type VARCHAR(35),
+       description TEXT,
        budget INT NOT NULL CHECK (budget between 1 and 3),
        energy_level INT NOT NULL CHECK (budget between 1 and 3),
        dog_friendly BOOLEAN NOT NULL DEFAULT NULL,
