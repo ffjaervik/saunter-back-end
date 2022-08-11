8ccd5ddd5e10db6230da7be15be89a4f8e24fb6d
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-08-11 15:25:53 +0100

    test: testing GET medium-budger, GET high-budget, GET all-budgets

--- test/activities.test.js
+++ test/activities.test.js
@@ -4,8 +4,6 @@ import {test, expect, describe} from '@jest/globals'
 import {pool} from '../db/index'
 
 
-// beforeAll/afterAll currently not closing the test after it's complete.
-
 
 describe("GET low-budget", function() {
     test("Get request (status code: 200) and check that header is json", async function(){
@@ -37,39 +35,38 @@ describe("GET low-budget", function() {
                 }
             ])
         };
-
-        // expect(actual).toMatchObject(expected);
         expect.objectContaining(expected)
     })
 })
 
 
-//     test("GET request with specific ID and correct body structure", async function(){
-//         const response = await request(app).get("/v1/mindactivity/4");
-//         const actual = response.body;
-//         const expected = {
-//             success: true,
-//             payload: [{
-//                 id: 4,
-//                 content: expect.any(String)
-//             }]
-//         };
-//         expect(actual).toEqual(expected);
-//     })
-// })
+describe("GET /medium-budget", function () {
+  it("responds with json", function (done) {
+    request(app)
+      .get("/medium-budget")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done);
+  });
 
-//   afterAll(async () => {
-//     await pool.end() 
-// });
+});
 
-// {"data": 
-// [{"budget": 1,
-//  "description": "The Natural History Museum in London is a museum that exhibits a vast range of specimens from various segments of natural history.",
-//   "dog_friendly": false, 
-//   "energy_level": 2,
-//    "id": 1, 
-//    "image": "https://media.timeout.com/images/105162351/image.jpg", 
-//    "name": "National Science Museum", 
-//    "saved": 
-// false, 
-// "type": "activity"
+describe("GET /high-budget", function () {
+  it("responds with json", function (done) {
+    request(app)
+      .get("/high-budget")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done);
+  });
+});
+
+describe("GET /all-budgets", function () {
+  it("responds with json", function (done) {
+    request(app)
+      .get("/all-budgets")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done);
+  });
+});
