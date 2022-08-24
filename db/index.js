d9274b8f558d8d19a2a47c5f97950a317f0658eb
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-08-24 10:48:04 +0100

    updating database credentials

--- db/index.js
+++ db/index.js
@@ -1,11 +1,12 @@
 
 import pg from 'pg';
 export const pool = new pg.Pool({
-  connectionString: "postgres://tjzldbdpqtlvyi:e2bf0a54d653c4ca7cfe7e99aca43735b72309b15176ee8283e487012c396cbe@ec2-52-30-75-37.eu-west-1.compute.amazonaws.com:5432/dcn7n0ovqpk8dr",
+  connectionString:
+    "postgres://hmsosdvhfkbofu:faf9e01c975b048388820d43eede8145e936300f0407f5d789b8ec6270d18a59@ec2-54-220-86-118.eu-west-1.compute.amazonaws.com:5432/d7176nhb6i9kj5",
 
   ssl: {
-    rejectUnauthorized: false
-  } ,
+    rejectUnauthorized: false,
+  },
 });
  export function query(text, params, callback){
   return pool.query(text, params, callback);
