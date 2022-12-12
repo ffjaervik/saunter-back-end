cbbfa886051aefe980285ab5a33e4efdef71ccf9
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-12-12 15:30:28 +0000

    changed cors

--- app.js
+++ app.js
@@ -6,16 +6,24 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import logger from "morgan";
 
-import router from "./routes/activities.js"
+import router from "./routes/activities.js";
 
 const app = express();
 
 app.use(logger("dev"));
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/", router);
 
@@ -38,9 +46,7 @@ app.use(function (err, req, res, next) {
 // });
 
 app.listen(process.env.PORT || 3002, () => {
-    console.log(`Example app listening on port ${process.env.PORT || 3002}`);
-} 
-);
-
+  console.log(`Example app listening on port ${process.env.PORT || 3002}`);
+});
 
 export default app;
